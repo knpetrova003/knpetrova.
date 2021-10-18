c08cca95570734f90108e1f4139cd13e103b7e03
K.N.Petrova@urfu.me <K.N.Petrova@urfu.me>
2021-10-18 11:09:44 +0300

    hw2

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,10 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите длину стороны:");
-            double a = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Объем икосаэдра:{(5 * (3 + Math.Sqrt(5)) * a * a * a) / 12}");
-            Console.WriteLine($"Площадь треугольника:{(5 * a * a * Math.Sqrt(3))}");
+            
             Console.ReadKey();
         }
     }
